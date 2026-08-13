4a021c03dfce0d139a86ffcedfdb9c820da1d67b
RadiantEgg <2173590735@qq.com>
2026-08-13 21:10:21 +0800

    完成input.h架构

---
+++ include/input.h
@@ -0,0 +1,31 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdio.h>
+
+typedef struct {
+    char **data;
+    size_t size;
+    size_t capacity;
+} LineList;
+
+LineList *linelist_create(void);
+
+void linelist_destroy(LineList *lst);
+
+void linelist_read_stdin(LineList *lst, const char *filename);
+
+void linelist_read_file(LineList *lst);
+
+// void linelist_add(LineList *lst, char *line);
+
+// char *read_line_from_stdin(LineList *lst);
+
+// char *read_line_from_file(LineList *lst);
+
+// void resize
+
+
+
+#endif
+
