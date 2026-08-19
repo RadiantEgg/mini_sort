91051247b0d0d18f252806c9c6449fdd2ca5e5a4
RadiantEgg <2173590735@qq.com>
2026-08-19 19:20:24 +0800

    完成compare模块设计，暂时枚举函数种类以生成cmp函数

---
+++ src/compare.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "compare.h"
+
+static Comparator cmp_numeric(const void *a, const void *b)
+{
+    double x = atof((const char *)a);
+    double y = atof((const char *)b);
+    if (x == y)
+        return 0;
+    else if (x > y)
+        return 1;
+    else
+        return -1;
+}
+
+static Comparator cmp_numeric_reverse(const void *a, const void *b)
+{
+    double x = atof((const char *)a);
+    double y = atof((const char *)b);
+    if (x == y)
+        return 0;
+    else if (x > y)
+        return -1;
+    else
+        return 1;
+}
+
+static Comparator cmp_dictionary(const void *a, const void *b)
+{
+    return strcmp((const char *)a, (const char *)b);
+}
+
+static Comparator cmp_dictionary_reverse(const void *a, const void *b)
+{
+    return -strcmp((const char *)a, (const char *)b);
+}
+
+static Comparator cmp_dictionary_foldcase(const void *a, const void *b)
+{
+    const char *s = (const char *)a;
+    const char *t = (const char *)b;
+    
+    while (*s && *t && tolower(*s) == tolower(*t))
+        s++, t++;
+    
+    if (*s == *t)
+        return 0;
+    
+    if (*s < *t)
+        return -1;
+    return 1;
+}
+
+static Comparator cmp_dictionary_foldcase_reverse(const void *a, const void *b)
+{
+    const char *s = (const char *)a;
+    const char *t = (const char *)b;
+    
+    while (*s && *t && tolower(*s) == tolower(*t))
+        s++, t++;
+    
+    if (*s == *t)
+        return 0;
+    
+    if (*s < *t)
+        return 1;
+    return -1;
+}
+
+Comparator compare_create(const SortConfig *cfg)
+{
+    if (cfg->flags & SORT_NUMERIC) {
+        if (cfg->flags & SORT_REVERSE)
+            return cmp_numeric_reverse;
+        else    
+            return cmp_numeric;
+    } else {
+        if (cfg->flags & SORT_FOLDCASE) {
+            if (cfg->flags & SORT_REVERSE)
+                return cmp_dictionary_foldcase_reverse;
+            else    
+                return cmp_dictionary_foldcase;
+        } 
+        else if (cfg->flags & SORT_REVERSE)
+            return cmp_dictionary_reverse;
+        else
+            return cmp_dictionary;
+    }
+}
